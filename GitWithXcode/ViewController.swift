7337ed94f1448f7adef458295dafeb0f65a826e4
Devashree <devashree9@gmail.com>
2018-04-24 19:02:10 +0530

    Added view will appear

--- GitWithXcode/ViewController.swift
+++ GitWithXcode/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //This is added from sample branch.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
 
 
 }
